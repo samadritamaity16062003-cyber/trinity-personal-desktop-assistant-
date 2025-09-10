dbb858f935f2ac125b540d44d3d5d714bcb6a4c8
Samadrita Maity <samadritamaity16062003@gmail.com>
2025-09-10 12:11:13 +0530

    Update clint.py

--- clint.py
+++ clint.py
@@ -1,22 +1,3 @@
-# from openai import OpenAI
-# client = OpenAI(
-#     api_key="sk-ijklmnop1234qrstijklmnop1234qrstijklmnop"
-# )
-
-# # Using Chat Completions API
-# completion = client.chat.completions.create(
-#     model="gpt-5",
-#     messages=[
-#         {"role": "system", "content": "You are an AI assistant named Trinity skilled in general tasks like Alexa or Google Assistant."},
-#         {"role": "user", "content": "What is coding?"}
-#     ]
-# )
-
-# # Correct way to access the response
-# print(completion.choices[0].message["content"])
-
-
-
 from google import genai
 from google.genai import types
 
